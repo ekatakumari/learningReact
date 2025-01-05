3052fb45856ab030e702a8a804c8db00465722be
printwel <artscraft456@gmail.com>
2025-01-05 17:07:34 +0530

    add

--- my-app/src/component/FaqComponent/FaqComponent.js
+++ my-app/src/component/FaqComponent/FaqComponent.js
@@ -1,28 +1,45 @@
-import React from 'react'
-import FaqItem from './FaqItem'
-import "./FaqComponent.css"
+import React from "react";
+import FaqItem from "./FaqItem";
+import "./FaqComponent.css";
 const FaqComponent = () => {
-    const faqJson=[
-        {
-          question: "How many bones does a cat have?",
-          answer: "A cat has 230 bones - 6 more than a human",
-        },
-        {
-          question: "How much do cats sleep?",
-          answer: "The average cat sleeps 12-16 hours per day",
-        },
-        {
-          question: "How long do cats live",
-          answer: "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
-        },
-      ]
+  const faqJson = [
+    {
+      question: "How many bones does a cat have?",
+      answer: "A cat has 230 bones - 6 more than a human",
+    },
+    {
+      question: "How much do cats sleep?",
+      answer: "The average cat sleeps 12-16 hours per day",
+    },
+    {
+      question: "How long do cats live",
+      answer:
+        "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+    },
+    {
+      question: "How long do cats live",
+      answer:
+        "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+    },
+    {
+      question: "How long do cats live",
+      answer:
+        "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+    },
+    {
+      question: "How long do cats live",
+      answer:
+        "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+    },
+  ];
   return (
-    <div className='faq-component'>
-       {faqJson.map((data,index)=>
-        <FaqItem faq={data} index={index}/>
-    )}
+    <div className="faq-component">
+      <h2 className="faq-heading">FAQ COMPONENT</h2>
+      {faqJson.map((data, index) => (
+        <FaqItem faq={data} index={index} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default FaqComponent
+export default FaqComponent;
--- my-app/src/component/FaqComponent/FaqItem.js
+++ my-app/src/component/FaqComponent/FaqItem.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./FaqComponent.css";
 const FaqItem = ({ faq, index }) => {
   const [isShow, setIsShow] = useState(false);
-  const handleToggle=()=>{
-    setIsShow(!isShow)
-  }
+  const handleToggle = () => {
+    setIsShow(!isShow);
+  };
   useEffect(() => {
     if (index == 0) {
       setIsShow(true);
@@ -14,12 +14,19 @@ const FaqItem = ({ faq, index }) => {
     <div>
       <div className="faq-question-answer">
         <div className="question">
-         <div>{faq.question}</div>
-          <button className={`arrow-button ${isShow ? "rotate" : ""}`}  onClick={handleToggle}></button>
+          <span>{index + 1}.</span>
+          <div>{faq.question}</div>
+          <button
+            className={`arrow-button ${isShow ? "rotate" : ""}`}
+            onClick={handleToggle}
+          ></button>
         </div>
-        <div className="answer">
-         {isShow && <div>{faq.answer}</div>}
-        </div>
+        {isShow && (
+          <div className="answer">
+           <img style={{width:"20px",height:"20px"}}src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJg6AN9J3CbverblDw1pHq7vZpkaicPMQ_sg&s"/>
+            <div>{faq.answer}</div>
+          </div>
+        )}
       </div>
     </div>
   );
