481dc0e3a60eb4317238df3394fd404c7986ed2f
ekata <ekata1995@gmail.com>
2025-03-29 19:21:07 +0530

    add

--- my-app/src/component/JobBoard/JobBoard.jsx
+++ my-app/src/component/JobBoard/JobBoard.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
+
 export const JobBoard=()=>{
+    const [result,setresult]=useState([])
+    useEffect(()=>{
+         apicall()
+    },[])
+    const apicall=async()=>{
+          const data=await fetch(" https://hacker-news.firebaseio.com/v0/jobstories.json")
+          const response=await data.json()
+          console.log(response)
+    }
+    const fetchData=()=>{
+        
+    }
    return <div className="job-board">
     <div className="container">
-               hello
+            
     </div>
 
    </div>
