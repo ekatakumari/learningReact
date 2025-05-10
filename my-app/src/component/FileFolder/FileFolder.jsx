ea3995bc6ada01a5d6daa6a26092a69a3483284e
ekatakumari <ekatakumari207@gmail.com>
2025-05-10 17:45:30 +0530

    add

--- my-app/src/component/FileFolder/FileFolder.jsx
+++ my-app/src/component/FileFolder/FileFolder.jsx
@@ -36,10 +36,23 @@ const FileFolder=()=>{
     
         setDataItem((prev) => addItem(prev));
     };
-    
+    const handleDeleteItem=(deletId)=>{
+        const updateItem=(item)=>{
+           return  item.filter((data)=>data.id!=deletId).map((item)=>{
+                if(item.children && item.children.length>0){
+                    return {
+                        ...item,
+                        children:updateItem(item.children)
+                    }
+                }
+                return item
+            })
+        }
+        setDataItem((prev)=>updateItem(prev))
+    }
     return <div className="file-folder-container"> 
           <div className="file-folder-list">
-               <FileList list={dataItem} handleAddItem={handleAddItem}/>
+               <FileList list={dataItem} handleAddItem={handleAddItem} handleDeleteItem={handleDeleteItem}/>
           </div>
     </div>
 }
